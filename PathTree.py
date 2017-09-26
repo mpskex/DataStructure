ea0de7eb44d8601228db3b1c07fefe1ca1cef794
刘  方瑞 <mpskex@163.com>
2017-09-26 23:54:13 +0800

    Fixed the Node Tree Structure

--- PathTree.py
+++ PathTree.py
@@ -33,12 +33,13 @@ class TreeNode(object):
     def __init__(self, data):
         self.child = []
         self.data = data
+        self.cost = 0
     def SetChild(self, child_list):
         self.child = child_list
     def AddChild(self, child):
         self.child.append(child)
-    def NodePrint(self):
-        return str(self.data) + " child:", self.child
+    def toString(self):
+        return str(self.data)
 
 class PathTree(object):
 
@@ -63,8 +64,7 @@ class PathTree(object):
         for n in child_list:
             temp_child_list = copy.deepcopy(child_list)
             temp_child_list.remove(n)
-            return_child_list.append(self.CreateTree(n, temp_child_list, lastleaf))
-        print node.NodePrint()
+            return_child_list.append(self.CreateTreeList(n, temp_child_list, lastleaf))
         self.Tree = [parent, return_child_list]
         return self.Tree
 
@@ -75,21 +75,34 @@ class PathTree(object):
     #       child_list:   child node list
     #       lastleaf:   last leaf to be fill
     def CreateTree(self, parent, child_list, lastleaf):
-        node = TreeNode(parent)
         if child_list == []:
-            return [parent, lastleaf]
+            return 
         return_child_list = []
         for n in child_list:
             temp_child_list = copy.deepcopy(child_list)
-            temp_child_list.remove(n)
-            node.AddChild(n)
-            print "--"
-            return_child_list.append(self.CreateTree(n, temp_child_list, lastleaf))
-        print node.NodePrint()
-        self.Tree = [parent, return_child_list]
-        return node
+            #print parent, "\t", parent.toString()
+            for k in temp_child_list:
+                if k.data==n.data:
+                    temp_child_list.remove(k)
+            #print temp_child_list
+            p = TreeNode(n.data)
+            parent.AddChild(p)
+            #print parent.child
+            self.CreateTree(p, temp_child_list, lastleaf)
+        return parent
+
+    #   pre-order traveler
+    #   --------------------------------------------
+    #   paras:
+    #       node:   root node in sub tree
+    def tLR(self, node, num):
+        print num, "\t: ", node.data
+        if node.child!=[]:
+            for i in node.child:
+                self.tLR(i, num+1)
 
 if __name__ == '__main__':
     pt = PathTree()
-    #print pt.CreateTree(0,[2,3,4], 5)[0]
-    pt.CreateTree(0,[2,3,4], 5)
+    #print pt.CreateTreeList(0,[2,3,4], 5)
+    tree = pt.CreateTree(TreeNode(0),[TreeNode(2),TreeNode(3),TreeNode(4)], TreeNode(5))
+    pt.tLR(tree, 1)
